809fbb3a7890ddf579d70eb59408ab692e6e575a
Andrei Popov <andrepopoff@gmail.com>
2021-01-05 11:06:00 +0200

    Show a toast message after logout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -54,6 +54,7 @@
 
 <script>
 import {email, required, minLength} from "vuelidate/lib/validators"
+import messages from "@/utils/messages"
 
 export default {
   name: 'Login',
@@ -64,6 +65,11 @@ export default {
   validations: {
     email: {email, required},
     password: {required, minLength: minLength(6)}
+  },
+  mounted() {
+    if (messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message])
+    }
   },
   methods: {
     submitHandler() {
