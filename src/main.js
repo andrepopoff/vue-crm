b7ee65873fa2c2076965fa70e165507f645e089c
Andrei Popov <andrepopoff@gmail.com>
2021-01-05 21:18:03 +0200

    Add firebase

--- src/main.js
+++ src/main.js
@@ -8,13 +8,33 @@ import messagePlugin from './utils/message.plugin'
 import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min';
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false;
 Vue.use(Vuelidate);
 Vue.use(messagePlugin);
 Vue.filter("date", dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+firebase.initializeApp({
+  apiKey: "AIzaSyAnVfA_13JII1kp17M7Hq1HIykQU9-Z-t8",
+  authDomain: "vue-crm-e540c.firebaseapp.com",
+  projectId: "vue-crm-e540c",
+  storageBucket: "vue-crm-e540c.appspot.com",
+  messagingSenderId: "730636656593",
+  appId: "1:730636656593:web:f7ad72f8744f6e79238af0",
+  measurementId: "G-FKTVSREZW8"
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount('#app');
+  }
+});
